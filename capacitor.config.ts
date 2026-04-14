f4c03c8ad12032c35374d9b35b0b6d0a3db449e8
raja <rohit83013@gmail.com>
2026-04-14 21:55:07 +0530

    chore: initialize Android project configuration files and update capacitor settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.conquer.store.app',
   appName: 'Conquer Store',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://10.14.159.113:3000',  // your local IP
+    cleartext: true,                  // needed for HTTP
+  },
 };
 
 export default config;
